5d76b9ea9ffc7ac19b063021a339fb81ca803d7e
huunghia.it11 <huunghia.it11@gmail.com>
2014-10-17 13:42:14 +0000

    rút gọn project

--- FirstJointJS/WebContent/js/stencil-groups.js
+++ FirstJointJS/WebContent/js/stencil-groups.js
@@ -86,12 +86,12 @@
 	var $app = $('#paper-holder-groups');
 	var paper = new joint.dia.Paper({
 		width : 800,
-		height : 800,
-		gridSize : 1,
+		height : 500,
+		gridSize : 5,
 		model : graph,
 		defaultLink : new joint.dia.Link({
 			labels: [
-			         { position: .5, attrs: { text: { text: 'label' } } }
+			         { position: .5, attrs: { text: { text: 'relationship' } } }
 			     ],
 			attrs : {
 				'.marker-target' : {
@@ -104,6 +104,8 @@
 		},
 		markAvailable : true
 	});
+	
+	
 
 	// inspector
 	var inspector;
@@ -128,9 +130,9 @@
 							text : {
 								text : {
 									type : 'text',
-									group : 'Labels',
+									group : 'Relationship',
 									index : 1,
-									label : "Label"
+									label : "Relationship name"
 								}
 							}
 						}
@@ -178,7 +180,7 @@
 						index : 3
 					},
 					labels : {
-						label : 'Labels',
+						label : 'Relationship',
 						index : 1
 					}
 				},
@@ -190,155 +192,15 @@
 
 	paper.on('cell:pointerdown', function(cellView) {
 		createInspector(cellView);
-	});
-
-	var r = new joint.shapes.basic.Rect({
-		position : {
-			x : 50,
-			y : 50
-		},
-		size : {
-			width : 70,
-			height : 40
-		},
-		attrs : {
-			rect : {
-				fill : '#2ECC71'
-			},
-			text : {
-				text : 'rect',
-				fill : 'black'
-			}
-		}
-	});
-
-	var c = new joint.shapes.basic.Circle({
-		position : {
-			x : 220,
-			y : 150
-		},
-		size : {
-			width : 80,
-			height : 40
-		},
-		attrs : {
-			circle : {
-				fill : '#9B59B6'
-			},
-			text : {
-				text : 'circle',
-				fill : 'white'
-			}
-		}
-	});
-
-	graph.addCells([ r, c ]);
-
-	// Show Inspector immediately for the rectangle so that it is visible to the
-	// reader straight away.
-
-	createInspector(paper.findViewByModel(r));
-
-	// auto layout2
-	function buildGraphFromAdjacencyList(adjacencyList) {
-
-		var elements = [];
-		var links = [];
-
-		_.each(adjacencyList, function(edges, parentElementLabel) {
-			elements.push(makeElement(parentElementLabel));
-
-			_.each(edges, function(childElementLabel) {
-				links.push(makeLink(parentElementLabel, childElementLabel));
-			});
-		});
-
-		// Links must be added after all the elements. This is because when the
-		// links
-		// are added to the graph, link source/target
-		// elements must be in the graph already.
-		return elements.concat(links);
-	}
-
-	function makeLink(parentElementLabel, childElementLabel) {
-
-		return new joint.dia.Link({
-			source : {
-				id : parentElementLabel
-			},
-			target : {
-				id : childElementLabel
-			},
-			attrs : {
-				'.marker-target' : {
-					d : 'M 4 0 L 0 2 L 4 4 z'
-				}
-			},
-			smooth : true
-		});
-	}
-
-	function makeElement(label) {
-
-		var maxLineLength = _.max(label.split('\n'), function(l) {
-			return l.length;
-		}).length;
-
-		// Compute width/height of the rectangle based on the number
-		// of lines in the label and the letter size. 0.6 * letterSize is
-		// an approximation of the monospace font letter width.
-		var letterSize = 8;
-		var width = 2 * (letterSize * (0.6 * maxLineLength + 1));
-		var height = 2 * ((label.split('\n').length + 1) * letterSize);
-
-		return new joint.shapes.basic.Rect({
-			id : label,
-			size : {
-				width : width,
-				height : height
-			},
-			attrs : {
-				text : {
-					text : label,
-					'font-size' : letterSize,
-					'font-family' : 'monospace'
-				},
-				rect : {
-					width : width,
-					height : height,
-					rx : 5,
-					ry : 5,
-					stroke : '#555'
-				}
-			}
-		});
-	}
-
-	$('#btn-layout2').on('click', layout);
-
-	function layout() {
-
-		try {
-			var adjacencyList = eval('adjacencyList = '
-					+ $('#adjacency-list').val());
-		} catch (e) {
-			alert(e);
-		}
-
-		var cells = buildGraphFromAdjacencyList(adjacencyList);
-		graph.resetCells(cells);
-		joint.layout.DirectedGraph.layout(graph, {
-			setLinkVertices : false
-		});
-	}
-	layout();
+	});	
+	
 
 	// layout 3
 	$('#btn-layout3').on('click', function() {
 
 		joint.layout.GridLayout.layout(graph, {
-			columns : 3,
-			columnWidth : 100,
+			columns : 10,
+			columnWidth : 10,
 			rowHeight : 70
 		});
 	});
@@ -423,6 +285,7 @@
 							quality : .7
 						});
 					});
+	$('#btn-print').on('click', function() { paper.print(); });
 	var graphLayout = new joint.layout.ForceDirected({
 		graph : graph,
 		width : 380,
@@ -443,8 +306,35 @@
 
 	$('#btn-layout').on('click', animate);
 
-	// scroll paper
+	//drag to link
+	paper.on('cell:pointerup', function(cellView, evt, x, y) {
 
+	    // Find the first element below that is not a link nor the dragged element itself.
+	    var elementBelow = graph.get('cells').find(function(cell) {
+	        if (cell instanceof joint.dia.Link) return false; // Not interested in links.
+	        if (cell.id === cellView.model.id) return false; // The same element as the dropped one.
+	        if (cell.getBBox().containsPoint(g.point(x, y))) {
+	            return true;
+	        }
+	        return false;
+	    });
+	    
+	    // If the two elements are connected already, don't
+	    // connect them again (this is application specific though).
+	    if (elementBelow && !_.contains(graph.getNeighbors(elementBelow), cellView.model)) {
+	        
+	        graph.addCell(new joint.dia.Link({
+	            source: { id: cellView.model.id }, target: { id: elementBelow.id },
+	            labels: [
+				         { position: .5, attrs: { text: { text: 'relationship' } } }
+				     ],
+	            attrs: { '.marker-source': { d: 'M 10 0 L 0 5 L 10 10 z' } }
+	        }));
+	        // Move the element a bit to the side.
+	        cellView.model.translate(0, 100);
+	    }
+	});
+	
 	// tuy chinh shape
 	paper.on('cell:pointerup', function(cellView) {
 		// We don't want a Halo for links.
@@ -454,7 +344,8 @@
 		var halo = new joint.ui.Halo({
 			graph : graph,
 			paper : paper,
-			cellView : cellView
+			cellView : cellView,
+			magnetFilter: function(magnetEl) { return $.contains(cellView.$('.inPorts')[0], magnetEl); }
 		});
 		halo.render();
 	});
@@ -489,6 +380,13 @@
 		paper : paper
 	});
 	snaplines.startListening();
+	$('#switch').change(function() {
+		if (this.checked) { 
+		    snaplines.startListening(); 
+		} else { 
+		    snaplines.stopListening(); 
+		}
+	    });
 
 	var stencil = new joint.ui.Stencil({
 		graph : graph,
@@ -496,9 +394,21 @@
 		width : 200,
 		height : 300,
 		groups : {
-			simple : {
-				label : 'Select Shapes',
+			rectgr : {
+				label : 'Select Rect',
 				index : 1
+			},
+			diamondgr : {
+				label : 'Select Diamond',
+				index : 2
+			},
+			circlegr : {
+				label : 'Select Circle',
+				index : 3
+			},
+			othergr : {
+				label : 'Select Others',
+				index : 4
 			}
 		}
 	});
@@ -513,49 +423,124 @@
 
 	$('#stencil-holder-groups').append(stencil.render().el);
 
-	var r = new joint.shapes.basic.Rect({
-		position : {
-			x : 20,
-			y : 10
-		},
-		size : {
-			width : 50,
-			height : 30
-		},
-		attrs : {
-			rect : {
-				fill : '#2ECC71'
-			},
-			text : {
-				text : 'rect',
-				fill : 'black'
-			}
-		}
+		
+	var rect = new joint.shapes.basic.Rect({
+        position: { x: 10, y: 10 },
+        size: { width: 70, height: 30 },
+        attrs: { rect: { width: 70, height: 30 } }
+    });
+    var rect2 = rect.clone();
+    rect2.translate(100);
+    var rect3 = rect.clone();
+    rect3.translate(0,50);
+    var rect4 = rect3.clone();
+    rect4.translate(100);
+    var rect5 = rect.clone();
+    rect5.translate(0,100);
+    
+    rect.attr({
+        rect: { fill: '#2C3E50', rx: 5, ry: 5, 'stroke-width': 2, stroke: 'black' },
+        text: {
+            text: 'name', fill: '#3498DB',
+            'font-size': 12, 'font-weight': 'bold', 'font-variant': 'small-caps', 'text-transform': 'capitalize'
+        }
+    });
+
+    rect2.attr({
+        rect: { fill: '#E74C3C', rx: 20, ry: 20, 'stroke-width': 0 },
+        text: {
+            text: 'name', fill: '#ECF0F1',
+            'font-size': 12, 'font-weight': 'normal', 'font-variant': 'small-caps', 'text-transform': 'capitalize'
+        }
+    });
+
+    rect3.attr({
+        rect: { fill: '#8E44AD', rx: 0, ry: 0, 'stroke-width': 0 },
+        text: {
+            text: 'name', fill: 'white',
+            'font-size': 12, 'font-weight': 'normal'
+        }
+    });
+
+    rect4.attr({
+        rect: { fill: '#2ECC71', rx: 0, ry: 0, 'stroke-width': 1, stroke: 'black', 'stroke-dasharray': '10,2' },
+        text: {
+            text: 'name', fill: 'black',
+            'font-size': 12, 'font-weight': 'normal'
+        }
+    });
+
+    rect5.attr({
+        rect: { fill: '#F39C12', rx: 20, ry: 20, 'stroke-width': 1, stroke: 'black', 'stroke-dasharray': '1,1' },
+        text: {
+            text: 'name', fill: 'gray',
+            'font-size': 12, 'font-weight': 'bold', 'font-variant': 'small-caps', 'text-transform': 'capitalize',
+            style: { 'text-shadow': '1px 1px 1px black' }
+        }
+    });
+    
+    
+    
+	var circle = new joint.shapes.basic.Circle({
+		position: { x: 10, y: 10 },
+        size: { width: 70, height: 30 },
+        attrs: { circle : { fill : '#9B59B6' } }
 	});
-	var c = new joint.shapes.basic.Circle({
-		position : {
-			x : 100,
-			y : 10
-		},
-		size : {
-			width : 50,
-			height : 30
-		},
-		attrs : {
-			circle : {
-				fill : '#9B59B6'
-			},
-			text : {
-				text : 'circle',
-				fill : 'white'
-			}
-		}
-	});
+	
+	var circle2 = circle.clone();
+    circle2.translate(100);
+    var circle3 = circle.clone();
+    circle3.translate(0,50);
+    var circle4 = circle3.clone();
+    circle4.translate(100);
+    var circle5 = circle.clone();
+    circle5.translate(0,100);
+    
+    circle.attr({
+        circle: { fill: '#2C3E50', rx: 5, ry: 5, 'stroke-width': 2, stroke: 'black' },
+        text: {
+            text: 'name', fill: '#3498DB',
+            'font-size': 12, 'font-weight': 'bold', 'font-variant': 'small-caps', 'text-transform': 'capitalize'
+        }
+    });
+
+    circle2.attr({
+        circle: { fill: '#E74C3C', rx: 20, ry: 20, 'stroke-width': 0 },
+        text: {
+            text: 'name', fill: '#ECF0F1',
+            'font-size': 12, 'font-weight': 'normal', 'font-variant': 'small-caps', 'text-transform': 'capitalize'
+        }
+    });
+
+    circle3.attr({
+        circle: { fill: '#8E44AD', rx: 0, ry: 0, 'stroke-width': 0 },
+        text: {
+            text: 'name', fill: 'white',
+            'font-size': 12, 'font-weight': 'normal'
+        }
+    });
+
+    circle4.attr({
+        circle: { fill: '#2ECC71', rx: 0, ry: 0, 'stroke-width': 1, stroke: 'black', 'stroke-dasharray': '10,2' },
+        text: {
+            text: 'name', fill: 'black',
+            'font-size': 12, 'font-weight': 'normal'
+        }
+    });
+
+    circle5.attr({
+        circle: { fill: '#F39C12', rx: 20, ry: 20, 'stroke-width': 1, stroke: 'black', 'stroke-dasharray': '1,1' },
+        text: {
+            text: 'name', fill: 'gray',
+            'font-size': 12, 'font-weight': 'bold', 'font-variant': 'small-caps', 'text-transform': 'capitalize',
+            style: { 'text-shadow': '1px 1px 1px black' }
+        }
+    });
 
 	var t = new joint.shapes.basic.Text({
 		position : {
-			x : 20,
-			y : 40
+			x : 10,
+			y : 10
 		},
 		size : {
 			width : 50,
@@ -571,7 +556,7 @@
 			{
 				position : {
 					x : 100,
-					y : 40
+					y : 70
 				},
 				size : {
 					width : 50,
@@ -612,42 +597,12 @@
 		}
 	});
 
-	var t = new joint.shapes.basic.Text({
-		position : {
-			x : 100,
-			y : 70
-		},
-		size : {
-			width : 50,
-			height : 30
-		},
-		attrs : {
-			text : {
-				text : 'Text'
-			}
-		}
-	});
-	var p = new joint.shapes.basic.Path(
-			{
-				position : {
-					x : 20,
-					y : 130
-				},
-				size : {
-					width : 50,
-					height : 50
-				},
-				attrs : {
-					path : {
-						d : 'M16,1.466C7.973,1.466,1.466,7.973,1.466,16c0,8.027,6.507,14.534,14.534,14.534c8.027,0,14.534-6.507,14.534-14.534C30.534,7.973,24.027,1.466,16,1.466z M14.757,8h2.42v2.574h-2.42V8z M18.762,23.622H16.1c-1.034,0-1.475-0.44-1.475-1.496v-6.865c0-0.33-0.176-0.484-0.484-0.484h-0.88V12.4h2.662c1.035,0,1.474,0.462,1.474,1.496v6.887c0,0.309,0.176,0.484,0.484,0.484h0.88V23.622z'
-					}
-				}
-			});
+	
 	var i = new joint.shapes.basic.Image(
 			{
 				position : {
 					x : 100,
-					y : 130
+					y : 10
 				},
 				size : {
 					width : 50,
@@ -664,7 +619,7 @@
 	var el1 = new joint.shapes.custom.ElementLabelLink({
 		position : {
 			x : 20,
-			y : 200
+			y : 150
 		},
 		size : {
 			width : 150,
@@ -682,7 +637,40 @@
 			}
 		}
 	});
+	
+	var diamond = new joint.shapes.basic.Path({
+		position : {
+			x : 10,
+			y : 10
+		},
+	    size: { width: 80, height: 50 },
+	    attrs: {
+	        path: { d: 'M 30 0 L 60 30 30 60 0 30 z' },
+	        text: {
+	            text: 'Diamond',
+	            'ref-y': .5 // basic.Path text is originally positioned under the element
+	        }
+	    }
+	});
+	
+	var hexagon = new joint.shapes.basic.Path({
+		position : {
+			x : 100,
+			y : 10
+		},
+	    size: { width: 80, height: 50 },
+	    attrs: {
+	    	path: { d: 'M 50 0 L 0 20 0 80 50 100 100 80 100 20 z'},
+	        text: {
+	            text: 'Hexagon',
+	            'ref-y': .5 // basic.Path text is originally positioned under the element
+	        }
+	    }
+	});
 
-	stencil.load([ r, c, t, p, i, m1, el1 ], 'simple');
+	stencil.load([ rect, rect2, rect3, rect4, rect5 ], 'rectgr');
+	stencil.load([ circle, circle2, circle3, circle4, circle5 ], 'circlegr');
+	stencil.load([ t, p, i, m1, el1 ], 'othergr');
+	stencil.load([ diamond , hexagon ], 'diamondgr');
 
 }())
--- FirstJointJS/WebContent/testJointJS.jsp
+++ FirstJointJS/WebContent/testJointJS.jsp
@@ -13,16 +13,15 @@
 	<header> <nav> <a>JointJS test by Group 11 - Class02
 		- JWD</a></nav> </header>
 
-	<!-- toàn bộ khung vẽ ở đây -->
+	<!-- TOAN BO KHUNG VE O DAY -->
 	<section id="rappid-docs">
 	<link rel="stylesheet" href="./css/joint.all.min.css">
-
-	<script src="./js/joint.all.min.js"></script> <script
-		src="./js/snaplines-style.js"></script> <script
-		src="./js/commandManager-create.js"></script>
+	<script src="./js/joint.all.min.js"></script>
 
 	<div>
+		<!--GROUP CAC BUTTON  -->
 		<div id="btn-gr">
+			<label>Snaplines</label><input type="checkbox" id="switch" checked />
 			<button class="btn" id="btn-reset">Reset</button>
 			<button class="btn" id="btn-undo">Undo</button>
 			<button class="btn" id="btn-redo">Redo</button>
@@ -34,66 +33,26 @@
 			<button class="btn" id="btn-open-svg">Save as SVG</button>
 			<button class="btn" id="btn-open-png">Save as PNG</button>
 			<button class="btn" id="btn-open-jpg">Save as JPG</button>
+			<button class="btn" id="btn-print">Print</button>
 		</div>
 		<div class="docs-demo">
 			<div id="stencil-holder-groups">
 				<div class="stencil">
-					<div class="stencil-paper-drag"></div>
+					
 					<div class="content">
-						<div class="group" data-name="simple"></div>
+						<div class="group" data-name="rectgr"></div>
+						<div class="group" data-name="diamondgr"></div>
+						<div class="group" data-name="circlegr"></div>
+						<div class="group" data-name="othergr"></div>
 					</div>
 				</div>
 			</div>
-
 			<div id="paper-holder-groups" class="paper"></div>
-			<div id="inspector-holder-create">
-					<div class="inspector">
-						<div class="group" data-name="labels">
-							<h3 class="group-label">Labels</h3>
-							
-							</div>
-						</div>
-						<div class="group" data-name="text">
-							<h3 class="group-label">Text</h3>
-							<div class="field" data-field="attrs/text/text">
-								<label>Text:</label>
-								<textarea class="textarea" data-type="textarea"
-									data-attribute="attrs/text/text">rect</textarea>
-							</div>
-							<div class="field" data-field="attrs/text/font-size">
-								<form
-									onchange="$(this).find(&#39;output&#39;).text(range.value)">
-									<label>Font size: (<output>14</output>)
-									</label> <input type="range" class="range" name="range"
-										data-type="range" data-attribute="attrs/text/font-size"
-										min="5" max="30" step="" value="14">
-								</form>
-							</div>
-						</div>
-						<div class="group" data-name="position">
-							<h3 class="group-label">Position</h3>
-							<div class="field" data-field="position/x">
-								<label>position/x:</label> <input type="number" class="number"
-									data-type="number" data-attribute="position/x" min="" max=""
-									step="" value="50">
-							</div>
-							<div class="field" data-field="position/y">
-								<label>position/y:</label> <input type="number" class="number"
-									data-type="number" data-attribute="position/y" min="" max=""
-									step="" value="50">
-							</div>
-						</div>
-					</div>
-				</div>
+			<div id="inspector-holder-create"></div>
 		</div>
 		<link rel="stylesheet" href="./css/style1.css">
-		<link rel="stylesheet" href="./css/stencil-groups.css" />
-		<link rel="stylesheet" href="./css/inspector-create.css">
-			
-
+		
 		<script src="./js/stencil-groups.js"></script>
-
-
 	</div>
 	</section>
 </body>
